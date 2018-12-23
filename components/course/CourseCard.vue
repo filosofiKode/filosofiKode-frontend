6fdfa3b76eb94f9b94230b3c072d11de74d49a35
dikyarga <dikyarga.id@gmail.com>
2018-12-23 23:51:39 +0700

    create course card component

---
+++ components/course/CourseCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+    />
+
+    <v-card-title primary-title>
+      <div>
+        <div class="headline">Top western road trips</div>
+        <span class="grey--text">1,000 miles of wonder</span>
+      </div>
+    </v-card-title>
+
+    <v-card-actions>
+      <v-btn flat>Share</v-btn>
+      <v-btn
+        flat
+        color="purple"
+      >Explore</v-btn>
+      <v-spacer/>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {}
+</script>
