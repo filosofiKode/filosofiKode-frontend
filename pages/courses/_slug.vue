807f596cdbd028034471655f4ab77f6f186cf01b
dikyarga <dikyarga.id@gmail.com>
2018-12-26 01:10:12 +0700

    make query by slug work

--- pages/courses/_slug.vue
+++ pages/courses/_slug.vue
@@ -1,3 +1,28 @@
 <template>
-  <h2>Hi</h2>
+  <div>
+    <h1>{{ course.title }}</h1>
+    <span>oleh {{ course.instructors[0].name }}</span>
+  </div>
 </template>
+<script>
+import courseBySlugQuery from '~/graphql/queries/course-by-slug'
+export default {
+  data() {
+    return {
+      course: {}
+    }
+  },
+  async asyncData({ app, params }) {
+    let client = app.apolloProvider.defaultClient
+    const { data } = await client.query({
+      query: courseBySlugQuery,
+      variables: {
+        slug: params.slug
+      }
+    })
+    return {
+      course: data.courses[0] // we get the one and only data
+    }
+  }
+}
+</script>
