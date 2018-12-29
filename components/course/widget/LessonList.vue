96034ebb8c7f734832eccde2af99bd6f7325fe1e
dikyarga <dikyarga.id@gmail.com>
2018-12-29 23:38:47 +0700

    create lesson list widget

---
+++ components/course/widget/LessonList.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-list two-line>
+    <v-subheader>
+      Playlist
+    </v-subheader>
+    <div
+      v-for="(lesson, index) in lessons"
+      :key="lesson._id">
+      <v-list-tile
+        nuxt
+        to="/"> 
+        <v-list-tile-action>
+          {{ index + 1 }}.
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ lesson.title }}</v-list-tile-title>
+          <v-list-tile-sub-title>4 menit</v-list-tile-sub-title>
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-divider inset/>
+    </div>
+  </v-list>
+</template>
+<script>
+export default {
+  name: 'LessonList',
+  props: {
+    lessons: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
