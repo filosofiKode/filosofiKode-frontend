6ccaed459dfae712685c3dc8649d0b029e6c6f27
dikyarga <dikyarga.id@gmail.com>
2018-12-30 15:41:43 +0700

    add lang http attribute

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,10 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
-    ]
+    ],
+    htmlAttrs: {
+      lang: 'id'
+    }
   },
 
   /*
